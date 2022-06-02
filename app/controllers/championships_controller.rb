9df4dcd6ae0e5022462a0aa8a6c04856dea25046
Alexey Kozhyn <suburbakskin@gmail.com>
2022-06-02 23:16:05 +0300

    Added bootstrap

--- app/controllers/championships_controller.rb
+++ app/controllers/championships_controller.rb
@@ -1,2 +1,5 @@
 class ChampionshipsController < ApplicationController
+  def index
+    @championships = Championship.order(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   devise_for :users
+  
   resources :championships
 
   root to: 'championships#index'
