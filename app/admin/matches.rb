38b5a8ab24b47c25dd3cd32ec4da1994c2a90478
Alexey Kozhyn <suburbakskin@gmail.com>
2022-06-03 12:35:15 +0300

    Added ActiveAdmin resource for Matches

---
+++ app/admin/matches.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Match do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :championship_id, :date, :team1, :team2, :score1, :score2, :team1_svm, :team2_svm, :draw_svm, :team1_prf, :team2_prf, :draw_prf, :team1_knn, :team2_knn, :draw_knn, :team1_k, :tea2_k, :draw_k, :played
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:championship_id, :date, :team1, :team2, :score1, :score2, :team1_svm, :team2_svm, :draw_svm, :team1_prf, :team2_prf, :draw_prf, :team1_knn, :team2_knn, :draw_knn, :team1_k, :tea2_k, :draw_k, :played]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
