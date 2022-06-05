5ee7d61e52086cd2a10a8a64074563182b7f2664
Alexey Kozhyn <suburbakskin@gmail.com>
2022-06-05 08:44:45 +0300

    Cteate account model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_060204) do
+ActiveRecord::Schema.define(version: 2022_06_05_053834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "sum"
+    t.integer "win"
+    t.integer "spend"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -99,6 +109,7 @@ ActiveRecord::Schema.define(version: 2022_06_04_060204) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "bets", "matches"
   add_foreign_key "bets", "users"
   add_foreign_key "matches", "championships"
